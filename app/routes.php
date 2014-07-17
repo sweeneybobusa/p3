27a7b3e688355389747804f4c16347be43c5e3e6
sweeneybobusa <sweeneybobusa@gmail.com>
2014-07-17 18:22:27 -0400

    uploaded lame excuse

--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,23 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+
+Route::get('/lorem-ipsum', function() {
+echo "Lorem Ipsum </br>";
+$default = 7;
+$generator = new Badcow\LoremIpsum\Generator();
+$paragraphs = $generator->getParagraphs($default);
+echo implode('<p>', $paragraphs);
+});
+
+Route::get('/user-generator', function() {
+echo "User Generator </br>";
+require_once '../vendor/fzaninotto/faker/src/autoload.php';
+$faker = Faker\Factory::create();
+
+for ($i=0; $i < 10; $i++) {
+	echo $faker->name, "\n" . "<br />";
+	echo $faker->address . "<br />";
+	echo $faker->text . "<br />";
+}
+});
